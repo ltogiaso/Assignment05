2e46154655d3e4f5eb923f24c8bd48210c2b21b6
ltogiaso <fiaalii.togiaso@gmail.com>
2025-11-27 06:12:13 -0800

    Final Commit

--- js/script.js
+++ js/script.js
@@ -1,1 +1,32 @@
-let theNextElementGet;
+const $ = (id) => document.getElementById(id)
+let form = $('emp-form')
+
+form.addEventListener('submit', (e) => {
+
+    e.preventDefault()
+
+    let children = e.target.children
+
+    for (let i = 0; i < children.length; i++){
+
+        let grandChildren = children[i].children
+        let stringToPrint= ''
+
+        for (let j = 0; j < grandChildren.length; j++){
+
+            if (grandChildren[j].tagName == 'LABEL')
+                stringToPrint += grandChildren[j].innerText + ': '
+            else if (grandChildren[j].tagName == 'INPUT')
+                stringToPrint += grandChildren[j].value
+            else if (grandChildren[j].tagName == 'SELECT')
+                stringToPrint += grandChildren[j].options[grandChildren[j].selectedIndex].text
+        }
+
+        console.log(stringToPrint)
+    }
+})
+
+//NOTE : Although I achieved the end result of the image from the example, 
+// I feel as if I did not fully utilize event object, nor have I utilized 
+// event propogation and delegation correctly. I would like to come back to
+// this at another time and go over this during office hours.
